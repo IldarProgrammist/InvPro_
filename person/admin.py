236916e3669451cb4503e4ac473e182be3ad9f14
Ильдар <programmerildarik@gmail.com>
2020-07-27 00:59:31 +0300

    Create new Project

--- person/admin.py
+++ person/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from person.models import Person
+from person.models import Person, Department, Position
 
 
 @admin.register(Person)
 class Person(admin.ModelAdmin):
-    list_display = ('firstName','lastName','fatherName', 'phone')
+    list_display = ('firstName','lastName','fatherName', 'phone','depatName','position')
+
+@admin.register(Department)
+class Depatment(admin.ModelAdmin):
+    list_display = ['depName']
+
+
+@admin.register(Position)
+class  Position(admin.ModelAdmin):
+    list_display = ['positionName']
+
+
+
--- person/models.py
+++ person/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 
-
-class Person(models.Model):
-    firstName = models.CharField(max_length=20, verbose_name='Фамилия')
-    lastName = models.CharField(max_length=20, verbose_name='Имя')
-    fatherName = models.CharField(max_length=20, verbose_name='Отчество')
-    phone = models.CharField(max_length=20, verbose_name='Телефон')
-    photo = models.ImageField( blank=True)
+class Position(models.Model):
+    positionName = models.CharField(max_length=30, verbose_name='Название должности')
 
     def __str__(self):
-        return self.firstName
+        return self.positionName
 
     class Meta:
-        verbose_name = 'Сотрудник'
-        verbose_name_plural = 'Сотрудники'
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
 
 
 
@@ -23,8 +18,25 @@ class Department(models.Model):
     def __str__(self):
         return self.depName
 
-
     class Meta:
         verbose_name = 'Отдел'
         verbose_name_plural = 'Отделы'
 
+
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=20, verbose_name='Фамилия')
+    lastName = models.CharField(max_length=20, verbose_name='Имя')
+    fatherName = models.CharField(max_length=20, verbose_name='Отчество')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    photo = models.ImageField( blank=True)
+    depatName = models.ForeignKey(Department, models.CASCADE, verbose_name='Отдел')
+    position = models.ForeignKey(Position,    models.CASCADE, verbose_name='Должность')
+
+
+    def __str__(self):
+        return self.firstName
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
